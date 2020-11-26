c25b3c8111d20fdd54c1652c3604b8083d873fc7
rclepf <rclepf@gmail.com>
2020-11-26 14:57:13 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ListaDeNotas from "./Components/ListaDeNotas";
+import FormularioCadastro from "./Components/FormularioCadastro";
+import ListaDeCategorias from "./Components/ListaDeCategorias";
+import "./assets/App.css";
+import './assets/index.css';
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias} />
+          <ListaDeNotas
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas} />
+        </main>
+      </section>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import CardNota from "../CardNota";
+import "./estilo.css";
+class ListaDeNotas extends Component {
+  constructor() {
+    super();
+    this.state = { notas: [] }
+  }
+
+  componentDidMount() {
+    this.props.notas.increver(this._novasNotas.bind(this))
+  }
+
+  _novasNotas(notas) {
+    this.setState({ ...this.state, notas })
+  }
+  render() {
+    return (
+      <ul className="lista-notas">
+        {this.state.notas.map((nota, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              <CardNota
+                indice={index}
+                apagarNota={this.props.apagarNota}
+                titulo={nota.titulo}
+                texto={nota.texto}
+                categoria={nota.categoria}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
